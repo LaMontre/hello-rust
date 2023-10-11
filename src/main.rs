89089ac49c0f4fed1b961b7f03a1aad098b77513
Anthony Urvoy <anthony.urvoy@stormaudio.com>
2023-10-11 09:29:45 +0200

    Add current_platform dependencie

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-// This is a comment, and is ignored by the compiler.
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
-// shortcut.
+use current_platform::{COMPILED_ON, CURRENT_PLATFORM};
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
-
-// This is the main function.
 fn main() {
-    // Statements here are executed when the compiled binary is called.
-
-    // Print text to the console.
-    println!("Hello World!");
+    println!("Hello, storm from {}! I was compiled on {}.", CURRENT_PLATFORM, COMPILED_ON);
 }
-
